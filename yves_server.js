ff49df5fb743c28ec0ce00ebe3ca4468426b6a5b
Joris Röling <j.roling@tmg.nl>
2017-11-19 16:03:32 +0100

    v1.0.21

--- yves_server.js
+++ yves_server.js
@@ -1,3 +1,7 @@
+if (!process.env.YVES_COLORS) process.env.YVES_COLORS='1'
+if (!process.env.FORCE_COLOR) process.env.FORCE_COLOR='1'
+if (!process.env.DEBUG_COLORS) process.env.DEBUG_COLORS='1'
+if (!process.env.DEBUG_HIDE_DATE) process.env.DEBUG_HIDE_DATE='1' 
+
 import lyves from 'yves'
 yves = lyves;
-// yves=yves.inspector({maxLength:-1,functions: false});
