e68c1338b1f58925efbb109de39196918c5dfdb6
Joris Röling <joris@bonboa.com>
2018-07-12 22:19:19 +0200

    v1.0.52

--- yves_client.js
+++ yves_client.js
@@ -1,19 +1,23 @@
 import lyves from 'yves/dist/yves.js'
 yves = lyves;
 
-// yves = Npm.require('yves')
-// debug = yves.debugger(Meteor.settings.public.name?Meteor.settings.public.name:'meteor')
-// yves.console(Meteor.settings.public.name?Meteor.settings.public.name:'meteor')
-
 var yvesHtml=yves.inspector({maxLength:-1,functions:false,html:true,colors:true});
 var counter=0;
 
-Template.registerHelper('yves', function(obj,hostPattern,absolute) 
+Template.registerHelper('yves', function(obj,hostPattern,absolute)
 {
+  // yves({obj,hostPattern,absolute})
 	if (hostPattern && typeof hostPattern=='string' && hostPattern.length && !new RegExp(hostPattern).test(window.location.hostname)) return '';
 	if (typeof hostPattern=='boolean' && !hostPattern) return '';
 	counter++;
-	var result='&nbsp;<i class="fa fa-eye" style="color:red;z-index:99999" onClick="$(\'.yves_'+counter+'\').toggle();return false;"></i><div class="yves_'+counter+'" style="display:none;clear:both;"></div><div class="yves_'+counter+'" style="display:none">'+yvesHtml(obj)+'</div>';
+  var cObj
+  if (typeof obj == "object") {
+    cObj = {}
+    Object.keys(obj).forEach(key => { cObj[key] = (typeof obj[key] == 'function') ? obj[key]() : obj[key] } )
+  } else {
+    cObj = obj;
+  }
+	var result='&nbsp;<i class="fa fa-eye" style="color:red;z-index:99999;cursor:pointer" onClick="$(\'.yves_'+counter+'\').toggle();return false;"></i><div class="yves_'+counter+'" style="display:none;clear:both;"></div><div class="yves_'+counter+'" style="display:none">'+yvesHtml(cObj)+'</div>';
 	if ((typeof absolute=='boolean' && absolute) || (typeof absolute=='string' && absolute=='absolute')) result='<div style="position:absolute;z-index:999999">'+result+'</div>';
 	return result;
 });
--- yves_server.js
+++ yves_server.js
@@ -1,15 +1,3 @@
-// if (!process.env.YVES_COLORS) process.env.YVES_COLORS='1'
-// if (!process.env.FORCE_COLOR) process.env.FORCE_COLOR='1'
-// if (!process.env.DEBUG_COLORS) process.env.DEBUG_COLORS='1'
-// if (!process.env.DEBUG_HIDE_DATE) process.env.DEBUG_HIDE_DATE='1'
-//
 import lyves from 'yves'
 yves = lyves;
 
-// yves = Npm.require('yves')
-// yves = require('yves')
-
-
-// debug = yves.debugger(Meteor.settings.public.name?Meteor.settings.public.name:'meteor')
-//
-// yves.console(Meteor.settings.public.name?Meteor.settings.public.name:'meteor')
