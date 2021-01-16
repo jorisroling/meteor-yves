48a6d205654153c221117940311843ff5187c016
Joris Röling <joris@bonboa.com>
2021-01-16 14:52:04 +0100

    v1.0.75

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jorisroling:yves',
-  version: '1.0.72',
+  version: '1.0.75',
   // Brief, one-line summary of the package.
   summary: 'Meteor implementation of (yves)[https://github.com/jorisroling/yves]',
   // URL to the Git repository containing the source code for this package.
@@ -11,14 +11,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2');
+  api.versionsFrom('1.7');
   api.use('ecmascript');
   api.addFiles('yves_server.js', 'server');
   api.addFiles('yves_client.js', 'client');
 	api.addFiles('yves_both.js');
-  api.use(['templating', 'spacebars', 'ui'], 'client');
+  api.use(['templating@1.3.2', 'spacebars@1.0.15', 'ui@1.0.13'], 'client');
   Npm.depends({
-    "yves": "1.0.72"
+    "yves": "1.0.75"
   });
   api.export('yves');
   // api.export('debug');
